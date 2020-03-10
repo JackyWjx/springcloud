20592ece11a729498b29e6641b9d47cdf674e5bf
wjx <2411329114@qq.com>
2020-03-10 17:51:14 +0800

    3.10  consul  error

---
+++ cloud-consumerconsul-order80/src/main/java/cn/wjx/springcloud/controller/OrderController.java
@@ -0,0 +1,23 @@
+package cn.wjx.springcloud.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class OrderController {
+    public static  final String INVOKE_URL = "http://consul-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consul/payment/consul")
+    public String paymentInfo() {
+        return restTemplate.getForObject(INVOKE_URL + "/payment/consul", String.class);
+    }
+}
--- cloud-provider-payment8001/src/main/java/cn/wjx/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/cn/wjx/springcloud/controller/PaymentController.java
@@ -6,13 +6,21 @@ import cn.wjx.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @Slf4j
 @RestController
 public class PaymentController {
     @Autowired
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private Integer port;
@@ -37,5 +45,19 @@ public class PaymentController {
         }else {
             return new CommonResult(404,"error");
         }
+    }
+
+    @GetMapping("/getDiscovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String service :
+                services) {
+            log.info(service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance: instances){
+            log.info("服务id:"+instance.getServiceId()+"\tIP地址："+instance.getHost()+"\t端口号:"+instance.getPort()+"url:"+instance.getUri());
+        }
+        return discoveryClient;
     }
 }
