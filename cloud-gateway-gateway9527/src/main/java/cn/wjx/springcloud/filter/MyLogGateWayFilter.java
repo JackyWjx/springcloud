2c82bd96cfdc695931c8899b7155b79c621f35da
wjx <2411329114@qq.com>
2020-03-22 22:34:49 +0800

    Gateway

---
+++ cloud-gateway-gateway9527/src/main/java/cn/wjx/springcloud/filter/MyLogGateWayFilter.java
@@ -0,0 +1,35 @@
+package cn.wjx.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("************come  in  MyLogGateWayFilter:"+new Date());
+        List<String> username = exchange.getRequest().getQueryParams().get("username");
+        if(username==null){
+            log.error("非法用户");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
