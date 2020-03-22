0e36905026b4214ccf01ca50196b2ceedb954818
wjx <2411329114@qq.com>
2020-03-22 21:44:44 +0800

    Gateway01

---
+++ cloud-gateway-gateway9527/src/main/java/cn/wjx/springcloud/config/GateWayConfig.java
@@ -0,0 +1,17 @@
+package cn.wjx.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GateWayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("baidu",r->r.path("/baiduUrl").uri("http://baidu.com")).build();
+        return routes.build();
+    }
+}
