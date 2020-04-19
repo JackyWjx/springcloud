931092bebe1ee3ffe3afcb12ea4e5997b3501ed6
JackyWjx <2411329114@qq.com>
2020-04-19 21:59:10 +0800

    4.19

--- cloud-consumer-order80/src/main/java/cn/wjx/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/cn/wjx/springcloud/controller/OrderController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import sun.plugin.com.DispatchClient;
 
 import javax.annotation.Resource;
 import java.net.URI;
---
+++ cloudalibaba-sentinel-server8401/src/main/java/cn/wjx/springcloud/controller/FlowLimitController.java
@@ -0,0 +1,31 @@
+package cn.wjx.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wjx
+ * @date 2020/4/19 18:12
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+    @GetMapping("/testA")
+    public String testA(){
+//        try {
+//            TimeUnit.MILLISECONDS.sleep(800);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return "testA-----";
+    }
+
+    @GetMapping("/testB")
+    public String testB(){
+        log.info(Thread.currentThread().getName() + "...testB ");
+        return "testB   -----";
+    }
+
+}
